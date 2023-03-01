47eaa9f555671dcd81e964e012b1c74aa78cc944
jkujo20 <jkujo20@yahoo.com>
2023-03-01 15:15:21 +0800

    Add Route lesson

--- lesson/routes/web.php
+++ lesson/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,111 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Note: Original code
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+//----------Routes return string------
+//Notes: You can return string rather than view
+
+// Route::get('/', function () {
+//     return "Hello World!";
+// });
+
+// Route::get('/home', function () {
+//     return "Home";
+// });
+
+// Route::get('/about', function () {
+//     return "About";
+// });
+
+
+
+//----------Different Routes
+//Note: double colon means its static method or properties
+/*
+Route::get();
+Route::post();
+Route::put(); //for update and edit, replace whole data
+Route::patch(); //for update and edit, replace partial data, not use often put is better
+Route::delete();
+Route::option();
+*/
+
+
+//----------Route match
+// Route::match(['get', 'post'], '/', function () {
+//     return 'Route Match GET and POST Request';
+// });
+
+//Will show error if GET request is access
+//Show error message
+// Route::match(['post'], '/', function () {
+//     return 'Route Match POST Request';
+// });
+
+//----------Route any
+// Route::any('/', function () {
+//     return 'Route Any';
+// });
+
+//----------Shorten Route call no function
+//Route::view('/welcome', 'welcome');
+
+//----------Redirection
+// Route::get('/', function () {
+//     return 'Redirected';
+// });
+
+// Route::redirect('/welcome', '/');
+
+
+//----------Request
+// Route::get(
+//     '/users',
+//     function (Request $request) {
+//         dd($request);
+//         return null;
+//     }
+// );
+
+//----------Response
+//Notes: Website for http status codes https://developer.mozilla.org/en-US/docs/Web/HTTP/Status 
+
+Route::get('/fetch-data', function () {
+    return response('Hello World!', 200)
+        ->header('Content-Type', 'text/plain');
+});
+
+//----------Response with parameter
+Route::get('/fetch-data/{id}', function ($id) {
+    return response($id, 200)
+        ->header('Content-Type', 'text/plain');
+});
+
+//----------Response with multiple parameter
+Route::get('/fetch-data/{id}/{page}', function ($id, $page) {
+    return response($id . ' ' . $page, 200)
+        ->header('Content-Type', 'text/plain');
+});
+
+
+//----------Response with json
+Route::get('/request-json', function () {
+    return response()
+        ->json(['first_name' => 'Juan', 'last_name' => 'Dela Cruz']);
+});
+
+
+//----------Response with download
+Route::get('/request-download', function () {
+    $path = public_path() . '/download-file.txt';
+    $name = 'download-file.txt';
+    $header = array('Content-Type: application/text-plain');
+
+    return response()->download($path, $name, $header);
 });
