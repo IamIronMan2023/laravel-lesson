de72c6878b51a3cbaaa50c36fb7ca7ba310d42e7
jkujo20 <jkujo20@yahoo.com>
2023-03-07 16:52:07 +0800

    Add demo codes

--- lesson/resources/views/employee/create.blade.php
+++ lesson/resources/views/employee/create.blade.php
@@ -3,17 +3,18 @@
 <h1>Create Employee</h1>
 
 <form action="{{ route('employee.store') }}" method="POST">
+    @csrf
     <p>
         <label>First Name</label>
-        <input type="text" name="first_name" required="required" />
+        <input type="text" name="first_name" />
     </p>
     <p>
         <label>Last Name</label>
-        <input type="text" name="last_name" required="required" />
+        <input type="text" name="last_name" />
     </p>
     <p>
         <label>Email Address</label>
-        <input type="email" name="email" required="required" />
+        <input type="email" name="email" />
     </p>
     <input type="submit" value="Save" />
 </form>
