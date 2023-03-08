527449c86fc1b2b649b44070a3368e78724d0823
jkujo20 <jkujo20@yahoo.com>
2023-03-08 18:53:54 +0800

    Add auth and layout

--- lesson/resources/views/employee/index.blade.php
+++ lesson/resources/views/employee/index.blade.php
@@ -1,42 +1,32 @@
-@include('partials.header')
-<style>
-    table,
-    th,
-    td {
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
-</style>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Employee List</h1>
+        <table class="table">
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Action</th>
+            </tr>
+
+            @if (Auth::user())
+                <a href="{{ route('employee.create') }}">New</a>
+            @endif
 
 
-<h1>Employee List</h1>
-<table style="width:50%">
-    <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-        <th>Action</th>
-    </tr>
-
-    @if (Auth::user())
-        <a href="{{ route('employee.create') }}">New</a>
-    @endif
-
-
-    @foreach ($employees as $employee)
-        <tr>
-            <td> {{ $employee->first_name }}</td>
-            <td>{{ $employee->last_name }}</td>
-            <td>{{ $employee->email }}</td>
-            <td>
-                <a href="{{ route('employee.show', ['id' => $employee->id]) }}">View</a>
-            </td>
-            {{-- <td><a href={{ '/employee/' . $employee->id }}>View</a></td> --}}
-            {{-- <td><a href="{{ url('employee/' . $employee->id) }}">View</a></td> --}}
-
-        </tr>
-    @endforeach
-</table>
-<div>{{ $employees->links() }}</div>
-
-@include('partials.footer')
+            @foreach ($employees as $employee)
+                <tr>
+                    <td> {{ $employee->first_name }}</td>
+                    <td>{{ $employee->last_name }}</td>
+                    <td>{{ $employee->email }}</td>
+                    <td>
+                        <a href="{{ route('employee.show', ['id' => $employee->id]) }}">View</a>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+        {{ $employees->links() }}
+    </div>
+@endsection
