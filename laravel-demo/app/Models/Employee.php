80dc2a4f86e79ecd79e3dec0551de641f0d8c32b
jkujo20 <jkujo20@yahoo.com>
2023-03-11 09:56:40 +0800

    Add fullname attribute in Employee model

--- laravel-demo/app/Models/Employee.php
+++ laravel-demo/app/Models/Employee.php
@@ -4,14 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Employee extends Model
 {
     use HasFactory;
 
-    protected $table = 'emp';
+    //    protected $table = 'emp';
 
     protected $fillable = ['first_name', 'last_name', 'email'];
 
     protected $hidden = ['created_at', 'updated_at'];
+
+
+    public function fullName(): Attribute
+    {
+        return new Attribute(
+            get: fn () => $this->first_name . ' ' . $this->last_name,
+        );
+    }
 }
--- laravel-demo/resources/views/employee/index.blade.php
+++ laravel-demo/resources/views/employee/index.blade.php
@@ -9,16 +9,14 @@
         <h1>Employee List</h1>
         <table class="table">
             <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
+                <th>Full Name</th>
                 <th>Email</th>
                 <th>Action</th>
             </tr>
 
             @foreach ($employees as $employee)
                 <tr>
-                    <td>{{ $employee->first_name }}</td>
-                    <td>{{ $employee->last_name }}</td>
+                    <td>{{ $employee->full_name }}</td>
                     <td>{{ $employee->email }}</td>
                     <td><a href="{{ route('employee.show', ['id' => $employee->id]) }}">View</a></td>
                 </tr>
