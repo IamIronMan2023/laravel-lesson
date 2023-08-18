bc331ec77ba21c57e8e4a9ef4b2393cd9bed17a3
IamIronMan2023 <kramsotnas@yahoo.com>
2023-08-18 18:53:29 +0800

    Add EmployeeRepositoryTest

---
+++ lesson2/tests/Feature/EmployeeRepositoryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Repositories\EmployeeRepository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EmployeeRepositoryTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_store(): void
+    {
+    }
+}
